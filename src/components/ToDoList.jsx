01b81bc5261184011983d259e8e146b1155a2371
Agung Rakhmat Gumilar <agungrg17@gmail.com>
2022-06-21 21:32:31 +0700

    modified to do list

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,18 +1,26 @@
+import { Checkbox, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import React from 'react';
 
-const ToDoList = ( {todos} ) => {
-
+const ToDoList = ({ todos }) => {
   return (
-    <div className='todo-list'>
-      <ul>
-        {
-          todos.map((item) => {
-            return <li key={item}> {item} </li>
-          })
-        }
-      </ul>
+    <div className="todo-list">
+      <List>
+        {todos.map((todo, index) => {
+          return (
+            <ListItem key={index} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Checkbox disableRipple/>
+                </ListItemIcon>
+                <ListItemText primary={todo} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
     </div>
   );
 };
 
 export default ToDoList;
+
