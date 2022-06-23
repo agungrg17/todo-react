9c9429c8a9c0dc4524e414734c0add04d40866c8
Agung Rakhmat Gumilar <agungrg17@gmail.com>
2022-06-23 22:04:50 +0700

    update form

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { Checkbox, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 
-const ToDoList = ( {todos} ) => {
+const ToDoList = ({ todos, handleCheckbox }) => {
 
   return (
-    <div className='todo-list'>
-      <ul>
-        {
-          todos.map((item) => {
-            return <li key={item}> {item} </li>
-          })
-        }
-      </ul>
+    <div className="todo-list">
+      <List>
+        {todos.map((todo) => {
+          return (
+            <ListItem key={todo.id} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Checkbox 
+                    checked={todo.complete} 
+                    onChange={() => handleCheckbox(todo.id)} 
+                    disableRipple />
+                </ListItemIcon>
+                <ListItemText 
+                  primary={todo.task}
+                  className={todo.complete ? "complete" : ""}
+                  // className={todo.complete && "complete"}
+                />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
     </div>
   );
 };
--- src/containers/ToDo.jsx
+++ src/containers/ToDo.jsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import {Button} from '@mui/material';
 
 import ToDoForm from '../components/ToDoForm';
 import ToDoList from '../components/ToDoList';
 
 const ToDo = () => {
+  const [todos, setTodos] = useState([
+    {id: 1, task: "menyapu", complete: false},
+    {id: 2, task: "mengepel", complete: true}
+  ]);
 
-  const [todos, setTodos] = useState(["Menyapu", "Mengepel"]);
+  const handleClick = (todo) => {
+    // NEVER mutate state directly, like using push() or =
+    // Treat state as if it were immutable, so you replace the whole array
+    // ...todos is the spread operator, it spreads the array
+    const newTodo = {
+      id: todos.length + 1, 
+      task: todo, 
+      complete: false}
+    setTodos([...todos, newTodo]);
+  };
 
-  const handleSubmit = (todo) => {
-    setTodos([...todos, todo]);
-    console.log(todos);
-    // alert("New Todo:" + todos);
+  const handleCheckbox = (id) => {
+    const newTodos = [...todos];
+    const index  = newTodos.findIndex(todo => todo.id === id);
+
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos)
+    // alert (`checkbox index ${id} is checked!`)
   }
-  
+
+  const removeCompleted = () => {
+    const newTodos = todos.filter(todo => todo.complete === false)
+    setTodos(newTodos)
+  }
 
   return (
-    <div className='todo'>
-      <h2 className='title'>To-Do List</h2>
-      <ToDoForm handleSubmit={handleSubmit}/>
-      <ToDoList todos={todos} />
+    <div className="todo">
+      <h2 className="title">To-Do List</h2>
+      <ToDoForm handleClick={handleClick} />
+      <ToDoList todos={todos} handleCheckbox={handleCheckbox} />
+      <Button variant='outlined' color='error' onClick={removeCompleted}>Remove Complete Task</Button>
     </div>
   );
 };
